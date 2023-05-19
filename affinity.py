bbdb46e10e186f97947fbc92da2db0f7290680bd
Ioannis Georvasilis <ioannis.georvasilis@gmail.com>
2023-05-19 11:29:45 +0300

    [UPDATE] affinity function

--- affinity.py
+++ affinity.py
@@ -1,14 +1,9 @@
+import warnings
 import numpy as np 
-import cv2 as cv 
-from create_graph import image_to_graph
 
-def affinity(G, u, v, sigma=1.):
-    # get the feature vector of node u
-    f_u = G.nodes[u]['feature']
-    # get the feature vector of node v
-    f_v = G.nodes[v]['feature']
-    # compute the affinity between u and v
-    return np.exp(
-        (-np.linalg.norm(f_u - f_v)**2) / (2 * sigma**2)
-        )
+# ignore runtime warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
+def affinity(pixel1, pixel2, sigma=4.):
+    # define affinity function 
+    return np.exp(np.linalg.norm(pixel1 - pixel2) / (2 * sigma**2))
